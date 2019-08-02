55377d3b2b836a8c759610b9f1012ce42313093a
saakshig12 <51923126+saakshig12@users.noreply.github.com>
2019-08-02 10:35:52 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,48 +8,51 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+
 // Data
 var openTables = [
-  {
-    routeName: "smithfamily",
-    name: "The Smiths",
-    phone: "phone number",
-    email: "email"
-  },
- 
+{
+  routeName: "smithfamily",
+  name: "The Smiths",
+  phone: "phone number",
+  email: "email"
+},
+
 ];
 
 // Routes
-app.get("/", function(req, res) {
-  res.send("Welcome to Bob's Burger Joint!");
+app.get("/", function (req, res) {
+res.send("Welcome to Mos Eisley Casino!");
 });
 
 // Displays all characters
-app.get("/api/makeReservations", function(req, res) {
-  return res.json(openTables);
+app.get("/api/makeReservations", function (req, res) {
+return res.json(openTables);
 });
+
 
 
 // Create New Tables - takes in JSON input
-app.post("/api/openTables", function(req, res) {
-    var waitingListArr = [];
-  var newCustomer = req.body;
-
-    for(i = 0; i < openTables.length; i++) {
-    if(i < 5){
-    openTables.push(newCustomer)
-    }
-    else{
-        waitingListArr.push(newCustomer)
-    }
+app.post("/api/openTables", function (req, res) {
+var waitingListArr = [];
+var newCustomer = req.body;
+if (openTables.length < 5) {
+  openTables.push(newCustomer);
 }
-  res.json(newCustomer);
-  console.log(openTables)
-  console.log(waitingListArr)
+else {
+  waitingListArr.push(newCustomer)
+  console.log(openTables);
+  console.log(waitingListArr);
+  console.log("This is the " + waitingListArr);
+  console.log("Occuped Tables: " + openTables);
+}
+
+res.json(newCustomer);
+
 });
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+app.listen(PORT, function () {
+console.log("App listening on PORT " + PORT);
 });
 
 
